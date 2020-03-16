6b6526172de3f789c3e07390df287677146bb3d8
FlorisKnight <floris.peters14@gmail.com>
2020-03-16 12:38:01 +0100

    chore: move images to assets, refactor index.ejs, make components

--- legacy/models/directories.js
+++ legacy/models/directories.js
@@ -7,9 +7,9 @@ const dbremote = "mongodb://admin:vlladmin01@ds011472.mlab.com:11472/heroku_m8ck
 const css = "/stylesheets/";
 const js = "/javascripts/";
 
-const img = "/images/";
-const avtimg = "/images/avatar/";
-const dtimg = "/images/data/";
+const img = "/images.done/";
+const avtimg = "/images.done/avatar/";
+const dtimg = "/images.done/data/";
 
 //TODO: the directory for the datasets has to be moved to secure database!
 const dt = "/datasets/";
@@ -17,7 +17,7 @@ const dt = "/datasets/";
 exports.dblocal = dblocal;
 exports.dbremote = dbremote;
 
-//TODO: for the static files (css, js and images) also needs a separate server?
+//TODO: for the static files (css, js and images.done) also needs a separate server?
 exports.css = css;
 exports.js = js;
 
