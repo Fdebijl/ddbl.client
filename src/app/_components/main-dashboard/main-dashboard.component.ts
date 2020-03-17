6fa6bf42fe8757ab02c951b92e18ae6c90cf3047
FlorisKnight <floris.peters14@gmail.com>
2020-03-17 11:39:32 +0100

    fix: main dashboard compiles, add metadata domain

--- src/app/_components/main-dashboard/main-dashboard.component.ts
+++ src/app/_components/main-dashboard/main-dashboard.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {Metadata} from '../../_domain/metadata';
 
 @Component({
   selector: 'app-main-dashboard',
   templateUrl: './main-dashboard.component.html',
   styleUrls: [
     './main-dashboard.component.scss',
-    'main.js'
   ]
 })
 export class MainDashboardComponent implements OnInit {
 
   constructor() { }
 
-  public data: Array<any>;
+  public data: Array<Metadata>;
 
   ngOnInit(): void {
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent, LoginComponent, RegisterComponent } from './_components';
-import { FooterComponent } from './_components/partials/footer/footer.component';
-import { HeaderComponent } from './_components/partials/header/header.component';
 import { MainDashboardComponent } from './_components/main-dashboard/main-dashboard.component';
 import { CardVisualisationComponent } from './_components/card-visualisation/card-visualisation.component';
 import { CardGenericComponent } from './_components/card-generic/card-generic.component';
