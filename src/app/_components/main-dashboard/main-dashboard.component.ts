a205cb398972a4409d69cc0200ee8e9a13975655
FlorisKnight <floris.peters14@gmail.com>
2020-03-27 10:07:00 +0100

    chore: add filter

--- src/app/_components/main-dashboard/main-dashboard.component.ts
+++ src/app/_components/main-dashboard/main-dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Metadata} from '../../_domain/metadata';
 import {MongodbService} from '../../_services/mongodb.service';
+import {Meta} from '@angular/platform-browser';
 
 declare function mp1(zoomLev, lat, lon);
 declare function mp2(zoomLev, lat, lon);
@@ -20,9 +21,15 @@ export class MainDashboardComponent implements OnInit {
 
   constructor(private newService: MongodbService, ) { }
 
-  public data: Array<Metadata>;
+  public showData: Array<Metadata>;
+  public allData: Array<Metadata>;
 
-  private loadMaps(){
+  public filterInput: string;
+  public filterCategory = '';
+  public filterType = '';
+  public filterAgeGroup = '';
+
+  private loadMaps() {
     mp1(11, 51.46, 5.450);
     mp2(11, 51.46, 5.450);
     mp3(11, 51.46, 5.450);
@@ -32,13 +39,38 @@ export class MainDashboardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.newService.GetMetadata().then(data => this.data = data);
+    this.newService.GetMetadata().then(data => {this.allData = data; this.showData = data});
     this.loadMaps();
   }
 
   public saveObjForVisualizationPage(id: string) {
-    const obj = this.data.find(x => x.id === id);
+    const obj = this.allData.find(x => x.id === id);
     localStorage.setItem('visData', JSON.stringify(obj));
+  }
+
+  public filterCards() {
+    const cardList = [];
+    const input = this.filterInput;
+    const cat = this.filterCategory;
+    const type = this.filterType;
+    const ageGroup = this.filterAgeGroup;
+    console.log('input: ' + input);
+    console.log('cat: ' + cat);
+    console.log('type: ' + type);
+    console.log('ageGroup: ' + ageGroup);
+    let i = 0;
+    for (const data of this.allData) {
+      console.log(i++);
+      console.log(data.category);
+      console.log(data.type);
+      console.log(data.ageGroup);
+      if ((data.category === cat || cat === '') && (data.type === type || type === '') && (data.ageGroup === ageGroup || ageGroup === '')
+        && (input === '' || input === undefined || data.title.includes(input))) {
+        cardList.push(data);
+      }
+    }
+    this.showData = cardList;
+    console.log('items: ' + this.showData.length);
   }
 
 }
