1551d88250de1fc63b3ca5ccc6cdbbc24730876c
FlorisKnight <floris.peters14@gmail.com>
2020-04-07 12:05:08 +0200

    chore: make register form, delete components

--- src/app/_components/user/register/register.component.ts
+++ src/app/_components/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterStateSnapshot  } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { AuthenticationService } from '../../../_services';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
+  redirectTo: string;
+  message = {
+    email: '',
+    displayName: '',
+    password: '',
+    bio: '',
+    affiliation: '',
+    generic: ''
+  };
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private authenticationService: AuthenticationService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    if (this.authenticationService.isAuthenticated()) {
+      this.router.navigate(['/home']);
+    }
+
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      displayName: ['', Validators.required],
+      password: ['', Validators.required],
+      bio: [''],
+      affiliation: ['']
+    });
   }
 
+  // convenience getter for easy access to form fields
+  get f(): { [key: string]: AbstractControl } {
+    return this.registerForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    this.loading = true;
+
+    this.authenticationService.login(this.f.username.value, this.f.password.value)
+      .then(() => {
+        if (this.redirectTo) {
+          this.router.navigate([this.redirectTo]);
+        } else {
+          this.router.navigate(['/']);
+        }
+      })
+      .catch(() => {
+        this.message.generic = 'Invalid username or password';
+
+        setTimeout(() => {
+          this.message.generic = null;
+        }, 10000);
+      })
+      .finally(() => {
+        this.submitted = false;
+        this.loading = false;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,10 @@ import { FooterComponent, HeaderComponent } from './_components/partials';
 import { ErrorComponent } from './_components/error/error.component';
 import { LoginComponent } from './_components/user/login/login.component';
 import { ProfileComponent } from './_components/user/profile/profile.component';
-import { SuccessComponent } from './_components/user/success/success.component';
 import { VisComponentComponent } from './_components/vis-component/vis-component.component';
 import {MongodbService} from './_services/mongodb.service';
 import { CardComponent } from './_components/card/card.component';
+import {RegisterComponent, UploadComponent} from './_components';
 
 @NgModule({
   declarations: [
