6f73d1a8c0bb513f670f0bc519ffc29e322dc245
FlorisKnight <25816363+FlorisKnight@users.noreply.github.com>
2020-05-27 11:50:25 +0200

    feat: add terms of service link to footer (#74)
    * feat: add terms of service page, add check box agree with tos register
    
    * feat: add terms of service to footer

--- src/app/_domain/class/data/SetMeta.ts
+++ src/app/_domain/class/data/SetMeta.ts
@@ -19,7 +19,7 @@ export class SetMeta {
 
   public static mock(): SetMeta {
     /* eslint-disable-next-line */
-    return JSON.parse(`{ "_id": { "$oid": "5de59e378da07a1aafe0ddf4" }, "created": { "$date": "2019-12-02T23:28:54.981Z" }, "title": "Sports Participation in Eindhoven", "year": "2017", "description": "This map shows the percentage of sports participation by district in Eindhoven in 2017.", "age_group": "All", "contributor": { "$oid": "5de596be2effde1a4c0276fb" }, "access": "Public", "type": "Visualization", "category": "Sports", "format": "JSON", "source": "Eindhovn Open Data, Eindhoven in Cijfers", "link": "https://data.eindhoven.nl/, https://www.eindhoven.nl/stad-en-wonen/stad/eindhoven-in-cijfers", "image": "error", "file": "mp1", "__v": 0, "share": [] }`);
+    return JSON.parse(`{"created":"2019-12-19T11:29:22.272Z","share":[],"_id":"5dfb5f12ea2a1308ed91ab8b","title":"Health and Diseases (19+)","year":"2009-2018","description":"This online dashboard enables you to compare the health and disease data (equal and more than 19 years old) between a specified municipality, region (Brabant Zuidoost) and province (Noord Brabant).","age_group":"19+","contributor":"5de596be2effde1a4c0276fb","access":"Public","type":"Web Page","category":"Health","format":"PDF/EXCEL/CSV","source":"GGD Brabantscan","link":"https://brabantscan.nl/","image":"5dfb5f12ea2a1308ed91ab8b","file":"https://brabantscan.nl/dashboard/Volwassenen--19--jaar-/GEZONDHEID-EN-ZIEKTEN/","__v":0}`);
   }
 }
 
