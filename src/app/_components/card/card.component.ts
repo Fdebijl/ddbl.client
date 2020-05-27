88b476ab0b5781d2d80a800aca52030ba54f51ce
FlorisKnight <25816363+FlorisKnight@users.noreply.github.com>
2020-05-27 12:21:55 +0200

    fix: remove visualization from cards (#76)
    * fix: remove visualization from cards
    
    * refactor: remove unneeded code

--- src/app/_components/card/card.component.ts
+++ src/app/_components/card/card.component.ts
@@ -2,12 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { SetMeta } from '../../_domain/class';
 import moment from 'moment';
 
-declare function mp1(zoomLev, lat, lon);
-declare function mp2(zoomLev, lat, lon);
-declare function mp3(zoomLev, lat, lon);
-declare function mp4(zoomLev, lat, lon);
-declare function ln1();
-
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -26,52 +20,6 @@ export class CardComponent implements OnInit {
 
   public saveObjForVisualizationPage(): void {
     localStorage.setItem('visData', JSON.stringify(this.cardData));
-  }
-
-  public updateAriaSelected(): void {
-      if (this.infoSelectAria === 'true') {
-        this.infoSelectAria = 'false';
-        this.cardInfoAria = 'true';
-        this.legendHiddenAria = 'false';
-      } else {
-        this.infoSelectAria = 'true';
-        this.cardInfoAria = 'false';
-        this.legendHiddenAria = 'true';
-      }
-      console.log('Update aria');
-  }
-
-  public getVisualizationTypeForClass(): string {
-    if (this.cardData.type === 'Visualization') {
-      return 'visualization';
-    } else {
-      return null;
-    }
-  }
-
-  private initForVisualization(): void {
-    this.cardInfoAria = 'true';
-    this.infoSelectAria = 'false';
-    switch (this.cardData.file) {
-      case 'mp1':
-        mp1(11, 51.46, 5.450);
-        break;
-      case 'mp2':
-        mp2(11, 51.46, 5.450);
-        break;
-      case 'mp3':
-        mp3(11, 51.46, 5.450);
-        break;
-      case 'mp4':
-        mp4(11, 51.46, 5.450);
-        break;
-      case 'mp5':
-        // mp5(11, 51.46, 5.450);
-        break;
-      case 'ln1':
-        ln1();
-        break;
-    }
   }
 
   public getIcon(): string {
@@ -98,9 +46,6 @@ export class CardComponent implements OnInit {
     this.cardInfoAria = 'false';
     this.infoSelectAria = 'true';
     this.legendHiddenAria = 'false';
-    if (this.cardData.type === 'Visualization') {
-      this.initForVisualization();
-    }
   }
 
 }
