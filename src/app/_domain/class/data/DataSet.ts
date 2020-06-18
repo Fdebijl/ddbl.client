4aa8bb4e9882815040267f6a52060d32da4df760
Floris de Bijl <18357629+Fdebijl@users.noreply.github.com>
2020-06-18 12:46:00 +0200

    feat: add constructor for DataSet

--- src/app/_domain/class/data/DataSet.ts
+++ src/app/_domain/class/data/DataSet.ts
@@ -1,10 +1,19 @@
-import { Data } from '../../interface/Data';
-import { Set } from './Set';
 import { DataType } from '../../enum/DataType';
 import { DataFormat } from '../../enum/DataFormat';
 import {MetaData} from './MetaData';
 
-export class DataSet extends Set {
+export class DataSet {
+  constructor(
+    {id, data, dataType, dataFormat, metaData}:
+    {id?: string; data?: string, dataType?: DataType; dataFormat?: DataFormat; metaData?: MetaData}
+  ) {
+    this.id = id;
+    this.data = data;
+    this.dataType = dataType;
+    this.dataformat = dataFormat;
+    this.metaData = metaData;
+  }
+
   id: string;
   data: string;
   dataType: DataType;
